3ea780bc5486976005a3659829787fc00607528b
Alex <titanaruto@gmail.com>
2019-07-28 14:59:40 +0300

    init work

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,27 @@
 var syntax        = 'sass', // выберете используемый синтаксис sass или scss, и перенастройте нужные пути в файле gulp.js и папки в вашего шаблоне wp
-		gulpversion   = '4'; // Выберете обязателньо свою версию Gulp: 3 или 4
+	gulpversion   = '4'; // Выберете обязателньо свою версию Gulp: 3 или 4
 
 var gulp          = require('gulp'),
-    autoprefixer  = require('gulp-autoprefixer'),
-    browsersync   = require('browser-sync'),
-    concat        = require('gulp-concat'),
-    cache         = require('gulp-cache'),
-    cleancss      = require('gulp-clean-css'),
-    ftp           = require('vinyl-ftp'),
-		imagemin      = require('gulp-imagemin'),
-		notify        = require('gulp-notify'),
-		pngquant      = require('imagemin-pngquant'),
-		gutil         = require('gulp-util' ),
-		rename        = require('gulp-rename'),
-		rsync         = require('gulp-rsync'),
-		sass          = require('gulp-sass'),
-		uglify        = require('gulp-uglify');
+	autoprefixer  = require('gulp-autoprefixer'),
+	browsersync   = require('browser-sync'),
+	concat        = require('gulp-concat'),
+	cache         = require('gulp-cache'),
+	cleancss      = require('gulp-clean-css'),
+	ftp           = require('vinyl-ftp'),
+	imagemin      = require('gulp-imagemin'),
+	notify        = require('gulp-notify'),
+	pngquant      = require('imagemin-pngquant'),
+	gutil         = require('gulp-util' ),
+	rename        = require('gulp-rename'),
+	rsync         = require('gulp-rsync'),
+	sass          = require('gulp-sass'),
+	uglify        = require('gulp-uglify');
 
-	
+
 // Незабываем менять 'wp-gulp.loc' на свой локальный домен
 gulp.task('browser-sync', function() {
 	browsersync({
-		proxy: "kirilovka.apteka.loc",
+		proxy: "mycalendar.loc",
 		notify: false,
 		// open: false,
 		// tunnel: true,
@@ -32,53 +32,115 @@ gulp.task('browser-sync', function() {
 
 // Обьединяем файлы sass, сжимаем и переменовываем
 gulp.task('styles', function() {
-	return gulp.src('src/sass/main.scss')
-	.pipe(sass({ outputStyle: 'expand' }).on("error", notify.onError()))
-	//.pipe(rename({ suffix: '.min', prefix : '' }))
-	.pipe(concat('style.min.css'))
-	.pipe(autoprefixer(['last 15 versions']))
-	.pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
-	.pipe(gulp.dest('src/css'))
-	.pipe(browsersync.stream())
+	return gulp.src('src/sass/**/*.scss')
+		.pipe(sass({ outputStyle: 'expand' }).on("error", notify.onError()))
+		//.pipe(rename({ suffix: '.min', prefix : '' }))
+		.pipe(concat('style.min.css'))
+		.pipe(autoprefixer(['last 15 versions']))
+		.pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
+		.pipe(gulp.dest('src/assets/css'))
+		.pipe(browsersync.stream())
 });
 
 
 // Обьединяем файлы скриптов, сжимаем и переменовываем
 gulp.task('scripts', function() {
 	return gulp.src([
-		'src/js/script.js',
-		])
-	.pipe(concat('scripts.min.js'))
-	// .pipe(uglify()) // Mifify js (opt.)
-	.pipe(gulp.dest('src/js'))
-	.pipe(browsersync.reload({ stream: true }))
+		'src/js/global.js',
+		'src/js/scripts.js',
+		//'src/wp-content/themes/twentyseventeen/assets/libs/jquery/dist/jquery.min.js', // Connecting my scripts
+		//'src/wp-content/themes/twentyseventeen/assets/js/common.js', // Always at the end
+	])
+		.pipe(concat('scripts.min.js'))
+		// .pipe(uglify()) // Mifify js (opt.)
+		.pipe(gulp.dest('src/assets/js'))
+		.pipe(browsersync.reload({ stream: true }))
 });
 
 
 // сжимаем картинки в папке images в шаблоне, и туда же возвращаем в готовом виде
 gulp.task('imgmin-theme', function() {
 	return gulp.src('src/images/**/*')
-	.pipe(cache(imagemin())) // Cache Images
-	.pipe(gulp.dest('src/images'));
+		.pipe(cache(imagemin())) // Cache Images
+		.pipe(gulp.dest('src/assets/images'));
 });
 
 
+// сжимаем картинки в папке uploads, и туда же возвращаем в готовом виде
+gulp.task('imgmin-uploads', function() {
+	return gulp.src('src/uploads/**/*')
+		.pipe(cache(imagemin())) // Cache Images
+		.pipe(gulp.dest('src/uploads'));
+});
+
+
+// Отгрузка всего сайта на хостинг
+gulp.task('deploy-site', function() {
+	var conn = ftp.create({
+		host:      '11.111.111.111', // or domain
+		user:      'user ftp',
+		password:  'password ftp',
+		parallel:  10,
+		log: gutil.log
+	});
+	var globs = [
+		'src/**', // Путь до готовой папки вашего сайта к отгрузки на хостинг
+		//'src/.htaccess',
+	];
+	return gulp.src(globs, {buffer: false})
+		.pipe(conn.dest('/www/domain.com/')); // Путь до папки сайта на хостинге
+});
+
+
+// Отгрузка только шаблона на хостинг
+gulp.task('deploy-theme', function() {
+	var conn = ftp.create({
+		host:      '11.111.111.111', // or domain
+		user:      'user ftp',
+		password:  'password ftp',
+		parallel:  10,
+		log: gutil.log
+	});
+	var globs = [
+		'src/wp-content/themes/twentyseventeen/**', // Путь до шаблона у вас на компьютере
+	];
+	return gulp.src(globs, {buffer: false})
+		.pipe(conn.dest('/www/domain.com/wp-content/themes/twentyseventeen/')); // Путь до шаблона на хостинге
+});
+
+
+// Отгрузка или всего сайта, или какой то папки по SSH, настроите нужный путь сами
+gulp.task('rsync', function() {
+	return gulp.src('src/**')
+		.pipe(rsync({
+			root: 'src/',
+			hostname: 'mycalendar.loc',
+			destination: 'mycalendar.loc',
+			// include: ['*.htaccess'], // Hidden files to include in the deployment
+			recursive: true,
+			archive: true,
+			silent: false,
+			compress: true
+		}));
+	// Documentation: https://pinchukov.net/blog/gulp-rsync.html
+});
+
 
 if (gulpversion == 3) {
-  gulp.task('watch', ['styles', 'scripts', 'browser-sync'], function() {
-	  gulp.watch(['src/sass/**/*.scss','src/css/*.css'], ['styles']); // Наблюдение за sass файлами в папке sass в теме
-	  gulp.watch(['src/js/**/*.js'], ['scripts']); // Наблюдение за JS файлами js в теме
-    gulp.watch('src/**/*.php', browsersync.reload) // Наблюдение за sass файлами php в теме
-  });
-  gulp.task('default', ['watch']);
+	gulp.task('watch', ['styles', 'scripts', 'browser-sync'], function() {
+		gulp.watch(['src/sass/**/*.scss'], ['styles']); // Наблюдение за sass файлами в папке sass в теме
+		gulp.watch(['src/**/*.js', 'src/js/global.js'], ['scripts']); // Наблюдение за JS файлами js в теме
+		gulp.watch('src/**/*.php', browsersync.reload) // Наблюдение за sass файлами php в теме
+	});
+	gulp.task('default', ['watch']);
 }
 
 
 if (gulpversion == 4) {
 	gulp.task('watch', function() {
 		gulp.watch('src/sass/**/*.scss', gulp.parallel('styles')); // Наблюдение за sass файлами в папке sass в теме
-		gulp.watch(['src/js/**/*.js'], gulp.parallel('scripts')); // Наблюдение за JS файлами в папке js
-    gulp.watch('src/**/*.php', browsersync.reload) // Наблюдение за sass файлами php в теме
+		gulp.watch(['src/**/*.js', 'src/js/global.js'], gulp.parallel('scripts')); // Наблюдение за JS файлами в папке js
+		gulp.watch('src/**/*.php', browsersync.reload) // Наблюдение за sass файлами php в теме
 	});
 	gulp.task('default', gulp.parallel('styles', 'scripts', 'browser-sync', 'watch'));
 }
--- src/views/includes/head.php
+++ src/views/includes/head.php
@@ -10,8 +10,7 @@ require 'config/config.php';
     <title>TEST BLOG</title>
     <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i"
           rel="stylesheet">
-    <link rel="stylesheet" href="/css/reset.css?ver=<?php echo VERSION; ?>"/>
-    <link rel="stylesheet" href="/css/style.css?ver=<?php echo VERSION; ?>"/>
+    <link rel="stylesheet" href="/assets/css/style.min.css?ver=<?php echo VERSION; ?>"/>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="/src/js/scripts.js?ver=<?php echo VERSION; ?>"></script>
+    <script src="/assets/js/scripts.min.js?ver=<?php echo VERSION; ?>"></script>
